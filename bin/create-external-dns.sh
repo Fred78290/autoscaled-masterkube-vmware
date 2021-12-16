13b8e4a1c3fddbf25c72dc3fcc8425896aca180e
fboltz <frederic.boltz@gmail.com>
2021-12-16 16:36:29 +0100

    FEAT: Set txt prefix to node group

--- bin/create-external-dns.sh
+++ bin/create-external-dns.sh
@@ -11,6 +11,7 @@ mkdir -p $ETC_DIR
 sed -e "s/__DOMAIN_NAME__/$DOMAIN_NAME/g" \
     -e "s/__GODADDY_API_KEY__/$GODADDY_API_KEY/g" \
     -e "s/__GODADDY_API_SECRET__/$GODADDY_API_SECRET/g" \
+    -e "s/__NODEGROUP_NAME__/$NODEGROUP_NAME/g" \
     $KUBERNETES_TEMPLATE/deploy.yaml > $ETC_DIR/deploy.yaml
 
 kubectl apply -f $ETC_DIR/deploy.yaml --kubeconfig=./cluster/${NODEGROUP_NAME}/config
