7482a6b449dc1c9bd6c884d79ea5f8fe8be07c92
fboltz <frederic.boltz@gmail.com>
2021-12-20 17:11:38 +0100

    Use local conf file for metal LB

--- bin/create-metallb.sh
+++ bin/create-metallb.sh
@@ -3,26 +3,15 @@ CURDIR=$(dirname $0)
 
 pushd $CURDIR/../
 
+export KUBERNETES_TEMPLATE=./templates/metallb
 export ETC_DIR=./config/${NODEGROUP_NAME}/deployment/metallb
 
 mkdir -p $ETC_DIR
 
-cat > $ETC_DIR/config.yml <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: metallb-system
-  name: config
-data:
-  config: |
-    address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-      - $METALLB_IP_RANGE
-EOF
+# https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
+# https://raw.githubusercontent.com/google/metallb/v0.11.0/manifests/metallb.yaml
+sed "s/__METALLB_IP_RANGE__/$METALLB_IP_RANGE/g" $KUBERNETES_TEMPLATE/metallb.yaml > $ETC_DIR/metallb.yaml
 
-kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config apply -f $KUBERNETES_TEMPLATE/namespace.yaml
 kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config apply -f $ETC_DIR/config.yml
-kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config apply -f https://raw.githubusercontent.com/google/metallb/v0.9.6/manifests/metallb.yaml
+kubectl --kubeconfig=./cluster/${NODEGROUP_NAME}/config apply -f $ETC_DIR/metallb.yaml
