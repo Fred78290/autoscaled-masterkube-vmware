17bcd338093161d26198767c43003dd06de534de
fboltz <frederic.boltz@gmail.com>
2022-10-06 12:14:44 +0200

    FIX: Don't archive pvc

--- bin/create-nfs-provisionner.sh
+++ bin/create-nfs-provisionner.sh
@@ -8,6 +8,8 @@ else
     helm upgrade -i --kubeconfig=${TARGET_CLUSTER_LOCATION}/config  -n kube-system \
         nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
         --set storageClass.name=${NFS_STORAGE_CLASS} \
+        --set storageClass.archiveOnDelete=false \
+        --set storageClass.onDelete=true \
         --set nfs.server=${NFS_SERVER_ADDRESS} \
         --set nfs.path=${NFS_SERVER_PATH}
 fi
