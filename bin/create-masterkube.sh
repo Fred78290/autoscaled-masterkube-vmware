f06a63601546ec14b72da0967ed50b42fe62f88b
fboltz <frederic.boltz@gmail.com>
2023-01-17 21:52:44 +0100

    FIX: abolsulte path not required

--- bin/create-masterkube.sh
+++ bin/create-masterkube.sh
@@ -126,7 +126,7 @@ function nextip()
     IP=$1
     IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | tr '.' ' '`)
     NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX + 1 ))`)
-    NEXT_IP=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | /usr/bin/sed -r 's/(..)/0x\1\ /g'`)
+    NEXT_IP=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1\ /g'`)
     echo "$NEXT_IP"
 }
 
