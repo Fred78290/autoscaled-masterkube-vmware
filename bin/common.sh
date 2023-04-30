ec02398ce194ce218829bf629c6d93435288f69c
Fred78290 <frederic.boltz@gmail.com>
2023-04-30 16:18:15 +0200

    FIX: Declare function before usage

--- bin/common.sh
+++ bin/common.sh
@@ -1,46 +1,3 @@
-for MANDATORY in kubectl govc jq yq cfssl
-do
-    if [ -z "$(command -v $MANDATORY)" ]; then
-        echo_red "The command $MANDATORY is missing"
-        exit 1
-    fi
-done
-
-if [ "$(uname -s)" == "Darwin" ]; then
-
-    if [ -z "$(command -v cfssl)" ]; then
-        echo_red_bold "You must install gnu cfssl with brew (brew install cfssl)"
-        exit 1
-    fi
-
-    if [ -z "$(command -v gsed)" ]; then
-        echo_red_bold "You must install gnu sed with brew (brew install gsed), this script is not compatible with the native macos sed"
-        exit 1
-    fi
-
-    if [ -z "$(command -v gbase64)" ]; then
-        echo_red_bold "You must install gnu base64 with brew (brew install coreutils), this script is not compatible with the native macos base64"
-        exit 1
-    fi
-
-    if [ ! -e /usr/local/opt/gnu-getopt/bin/getopt ]; then
-        echo_red_bold "You must install gnu gnu-getopt with brew (brew install coreutils), this script is not compatible with the native macos base64"
-        exit 1
-    fi
-
-    shopt -s expand_aliases
-
-    alias base64=gbase64
-    alias sed=gsed
-    alias getopt=/usr/local/opt/gnu-getopt/bin/getopt
-
-    TZ=$(sudo systemsetup -gettimezone | awk -F: '{print $2}' | tr -d ' ')
-    ISODIR=~/.local/vmware/cache/iso
-else
-    TZ=$(cat /etc/timezone)
-    ISODIR=~/.local/vmware/cache
-fi
-
 function verbose() {
     if [ ${VERBOSE} = "YES" ]; then
         eval "$1"
@@ -98,3 +55,47 @@ function echo_separator() {
 function echo_line() {
 	echo_grey "============================================================================================================================="
 }
+
+if [ "$(uname -s)" == "Darwin" ]; then
+
+    if [ -z "$(command -v cfssl)" ]; then
+        echo_red_bold "You must install gnu cfssl with brew (brew install cfssl)"
+        exit 1
+    fi
+
+    if [ -z "$(command -v gsed)" ]; then
+        echo_red_bold "You must install gnu sed with brew (brew install gsed), this script is not compatible with the native macos sed"
+        exit 1
+    fi
+
+    if [ -z "$(command -v gbase64)" ]; then
+        echo_red_bold "You must install gnu base64 with brew (brew install coreutils), this script is not compatible with the native macos base64"
+        exit 1
+    fi
+
+    if [ ! -e /usr/local/opt/gnu-getopt/bin/getopt ]; then
+        echo_red_bold "You must install gnu gnu-getopt with brew (brew install coreutils), this script is not compatible with the native macos base64"
+        exit 1
+    fi
+
+    shopt -s expand_aliases
+
+    alias base64=gbase64
+    alias sed=gsed
+    alias getopt=/usr/local/opt/gnu-getopt/bin/getopt
+
+    TZ=$(sudo systemsetup -gettimezone | awk -F: '{print $2}' | tr -d ' ')
+    ISODIR=~/.local/vmware/cache/iso
+else
+    TZ=$(cat /etc/timezone)
+    ISODIR=~/.local/vmware/cache
+fi
+
+for MANDATORY in kubectl govc jq yq cfssl
+do
+    if [ -z "$(command -v $MANDATORY)" ]; then
+        echo_red "The command $MANDATORY is missing"
+        exit 1
+    fi
+done
+
