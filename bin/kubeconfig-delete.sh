4dabfd34ceee4e44d5d3eca8a8f8bd89cc50b797
Frederic BOLTZ <frederic.boltz@gmail.com>
2023-12-26 14:21:02 +0100

    FIX: Delete user too

--- bin/kubeconfig-delete.sh
+++ bin/kubeconfig-delete.sh
@@ -3,7 +3,11 @@
 if [ "$#" -eq "2" ]; then
     kubectl config delete-context k8s-$1-admin@$2
     kubectl config delete-cluster $2
+    kubectl config delete-user k8s-$1-admin
+    kubectl config delete-user k8s-$1-admin@$2
 else
     kubectl config delete-context k8s-$1-admin@k8s-$1
     kubectl config delete-cluster k8s-$1
+    kubectl config delete-user k8s-$1-admin
+    kubectl config delete-user k8s-$1-admin@k8s-$1
 fi
